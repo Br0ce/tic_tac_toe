b20925b4207dc21513df9750ddd5a7b7ef2d74ca
br0ce <beck@informatik.uni-bonn.de>
2016-10-31 20:40:04 +0100

    fitted main to use Front_win Session_handler and Socket

--- ttt_frontend/src/main.cpp
+++ ttt_frontend/src/main.cpp
@@ -2,14 +2,16 @@
 #include <QMainWindow>
 
 #include "../hdr/front_win.h"
+#include "../hdr/session_handler.h"
 
 
 int main(int argc, char** argv)
 {
   QApplication app(argc, argv);
 
-  Front_win w;
-  w.show();
+  Front_win* gui = new Front_win;
+  gui->show();
+  Session_handler handler(gui);
 
   return app.exec();
 }
