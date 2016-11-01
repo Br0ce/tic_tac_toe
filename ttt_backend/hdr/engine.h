eca636a9a80a4a275f727d73084613649d5e49fb
br0ce <beck@informatik.uni-bonn.de>
2016-11-01 16:53:27 +0100

    extended Engine, next_move()

--- ttt_backend/hdr/engine.h
+++ ttt_backend/hdr/engine.h
@@ -23,7 +23,14 @@
 #ifndef ENGINE_H
 #define ENGINE_H
 
+#include <iostream>
+#include <vector>
+
 #include <QObject>
+
+#include "../hdr/pitch.h"
+#include "../hdr/action.h"
+#include "defines.h"
 
 
 class Engine : public QObject
@@ -35,7 +42,26 @@ public:
 
   explicit Engine(QObject* parent = Q_NULLPTR);
 
+  Action compute_next_move(const Player& p);
+  Index best_move();
+  std::vector<Index> actions();
+  Value get_value(const Player& p) const;
+
+signals:
+
+  void next_move_signal(QByteArray);
+
+public slots:
+
+  void incoming_inquiry(QByteArray a);
+
 private:
+
+  void info(const char* c)
+  { std::cout << c << "\n"; }
+
+  Pitch pitch_;
+
 };
 
 #endif // ENGINE_H
--- ttt_backend/src/engine.cpp
+++ ttt_backend/src/engine.cpp
@@ -24,5 +24,85 @@
 #include "../hdr/engine.h"
 
 Engine::Engine(QObject* parent) :
-  QObject(parent)
-{}
+  QObject(parent),
+  pitch_()
+{
+  info("Engine started");
+}
+
+void Engine::incoming_inquiry(QByteArray a)
+{
+  info("processing ...");
+
+  try
+  {
+    pitch_.set_pitch(a.constData());
+    info("inquiry correct: ");
+    info(a.constData());
+    const auto a = QByteArray::number(best_move());
+    emit next_move_signal(a);
+  }
+  catch(std::exception& e)
+  {
+
+  }
+}
+
+Action Engine::compute_next_move(const Player& p)
+{
+  if(pitch_.is_win(Player::ai))
+    return Action(get_value(Player::ai), 0);
+
+  if(pitch_.is_win(Player::user))
+    return Action(get_value(Player::user), 0);
+
+  if(pitch_.is_draw())
+    return Action(0, 0);
+
+  Action a;
+  for(const auto& id : actions())
+  {
+    if(pitch_.is_free(id))
+    {
+      pitch_.move(id, p);
+
+      if(p == Player::user)
+      {
+        Action tmp(compute_next_move(Player::ai));
+        tmp.set_index(id);
+        a = std::min(a, tmp);
+      }
+      else
+      {
+        Action tmp(compute_next_move(Player::user));
+        tmp.set_index(id);
+        a = std::max(a, tmp);
+      }
+      pitch_.un_move(id);
+    }
+  }
+
+  return a;
+}
+
+Index Engine::best_move()
+{
+  return compute_next_move(Player::ai).get_index();
+}
+
+std::vector<Index> Engine::actions()
+{
+  std::vector<Index> tmp(8);
+  for(Index i = 0; i < Pitch_size; ++i)
+    tmp.at(i) = i;
+
+  return tmp;
+}
+
+Value Engine::get_value(const Player& p) const
+{
+  if(p == Player::ai)
+    return 10 - pitch_.get_depth();
+  else
+    return -(10 - pitch_.get_depth());
+}
