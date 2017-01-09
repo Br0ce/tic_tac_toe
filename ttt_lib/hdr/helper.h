b9104d61c98592a884a5521309f8f0e3fd68def5
br0ce <beck@informatik.uni-bonn.de>
2017-01-09 08:07:24 +0100

    added hlp::is_win() and hlp::is_draw

--- ttt_lib/hdr/helper.h
+++ ttt_lib/hdr/helper.h
@@ -1,16 +1,38 @@
-//
-// Created by br0ce on 23.11.16.
-//
+/** @file helper.h
+ *
+ * @brief Collection of helping-fkt
+ *
+ * Copyright (C) 2016  Niklas Beck, beck@informatik.uni-bonn.de
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
 
 #ifndef HELPER_H
 #define HELPER_H
 
 #include <iostream>
+#include <string>
+
+#include "defines.h"
 
 namespace hlp
 {
 void info(const char* c);
+bool is_win(const Player& p, const std::string& s);
+bool is_draw(const std::string& s);
 };
-
 
 #endif //HELPER_H
--- ttt_lib/src/helper.cpp
+++ ttt_lib/src/helper.cpp
@@ -1,11 +1,65 @@
-//
-// Created by br0ce on 23.11.16.
-//
+/** @file helper.cpp
+ *
+ * @brief Collection of helping-fkt
+ *
+ * Copyright (C) 2016  Niklas Beck, beck@informatik.uni-bonn.de
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
 
 #include "../hdr/helper.h"
 
 
 void hlp::info(const char* c)
+{ std::cout << c << "\n"; }
+
+
+bool hlp::is_win(const Player& p, const std::string& s)
 {
-  std::cout << c << "\n";
+  char c = 'o';
+
+  if(p == Player::user)
+    c = 'x';
+
+  if(s.at(0) == c  && s.at(1) == c  && s.at(2) == c)
+    return true;
+
+  if(s.at(0) == c  && s.at(3) == c  && s.at(6) == c)
+    return true;
+
+  if(s.at(0) == c  && s.at(4) == c  && s.at(8) == c)
+    return true;
+
+  if(s.at(1) == c  && s.at(4) == c  && s.at(7) == c)
+    return true;
+
+  if(s.at(2) == c  && s.at(5) == c  && s.at(8) == c)
+    return true;
+
+  if(s.at(2) == c  && s.at(4) == c  && s.at(6) == c)
+    return true;
+
+  if(s.at(3) == c  && s.at(4) == c  && s.at(5) == c)
+    return true;
+
+  if(s.at(6) == c  && s.at(7) == c  && s.at(8) == c)
+    return true;
+
+  return false;
 }
+
+bool hlp::is_draw(const std::string& s)
+{ return (s.find("-") == std::string::npos); }
